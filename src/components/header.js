542e5cef89b35f3f94c2e120de3371d9f3c6d521
alexbitsong <89553342+scorpion237@users.noreply.github.com>
2023-11-17 20:40:23 +0100

    2e commit

--- src/components/header.js
+++ src/components/header.js
@@ -19,7 +19,7 @@ function AppHeader() {
             <Nav.Link as={Link} to="/teams">Teams</Nav.Link>
             <Nav.Link as={Link} to="/testimonials">Testimonials</Nav.Link>
             <Nav.Link as={Link} to="/pricing">Pricing</Nav.Link>
-            <Nav.Link as={Link} to="/blog">Blog</Nav.Link>
+            {/* <Nav.Link as={Link} to="/blog">Blog</Nav.Link> */}
             <Nav.Link as={Link} to="/contact">Contact</Nav.Link>
           </Nav>
         </Navbar.Collapse>
